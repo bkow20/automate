761dd9d6f82178ff981f115c21c989f68ad01f70
benkowitt <ben.kowitt@yourbind.com>
2020-05-03 09:01:18 -0400

    updates for deploy

--- site/myapp.py
+++ site/myapp.py
@@ -3,6 +3,8 @@ import stockquotes
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "comments.db"))
 
@@ -42,5 +44,5 @@ def manage():
 	return render_template('manage.html', comments=comments, americanPrice=americanPrice)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=False)
 
